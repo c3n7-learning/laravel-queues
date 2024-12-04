e63cf00f317f249a88cf09c974757d0956258384
Timothy Karani <kartimothy@gmail.com>
2024-12-04 18:48:29 +0300

    Done with configuring jobs

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    \App\Jobs\SendWelcomeEmail::dispatch();
+    // To delay dispatching by 5 sec
+    // \App\Jobs\SendWelcomeEmail::dispatch()->delay(5);
+
+    foreach (range(1, 10) as $i) {
+        \App\Jobs\SendWelcomeEmail::dispatch();
+    }
+
+    \App\Jobs\ProcessPayment::dispatch()->onQueue('payments');
 
     return view('welcome');
 });
